637b0f296e90606ba6776d768043ac221647040b
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2025-12-12 22:19:46 -0800

    Cycle detection.

--- crates/vexcore/src/dep_graph.rs
+++ crates/vexcore/src/dep_graph.rs
@@ -1,9 +1,12 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 use syn::Ident;
 
 
-
+/// Dependency Graph Node.
+/// Contains a set of dependencies, and a set of
+/// dependents, representing edges in a dependency
+/// graph.
 #[derive(Default)]
 struct DepNode<'a> {
     dependencies: HashSet<&'a Ident>,
@@ -28,7 +31,105 @@ impl<'a> DepGraph<'a> {
         node_mut.extend(dependencies);
     }
     
-    // pub fn build(&self) -> 
+    // /// Ensure that there are no cyclic dependencies.
+    // pub fn verify(&self) -> Result<(), Vec<&'a Ident>> {
+    //     let mut visited = HashSet::with_capacity(self.nodes.len());
+    //     let mut path = Vec::new();
+        
+    //     for (&ident, deps) in self.nodes.iter() {
+    //         path.clear();
+    //         path.push(ident);
+            
+    //     }
+    //     todo!()
+    // }
+    
+    pub fn topological_sort(&self) -> Result<Vec<&'a Ident>, ()> {
+        // By this point, the dependency graph has only been built with
+        // nodes that have their edges defined by dependencies, but not
+        // dependents.
+        // This topological sort algorithm depends on knowing a node's
+        // dependencies AND dependents.
+        // A new graph needs to be rebuilt, by stepping through the
+        // pre-built graph and iterating over each node(N)'s dependencies,
+        // and inserting node(n) into the dependency node's dependents set.
+        let mut full_graph = HashMap::new();
+        for (&key, dependencies) in self.nodes.iter() {
+            full_graph.entry(key).or_insert_with(|| DepNode {
+                dependencies: HashSet::with_capacity(dependencies.len()),
+                dependents: HashSet::new(),
+            }).dependencies.extend(dependencies.iter().copied());
+            for dep in dependencies.iter().copied() {
+                let dep_node = full_graph.entry(dep).or_insert_with(|| DepNode {
+                    dependencies: HashSet::new(),
+                    dependents: HashSet::new(),
+                });
+                dep_node.dependents.insert(key);
+            }
+        }
+        let mut order = Vec::with_capacity(full_graph.len());
+        let mut queue = VecDeque::with_capacity(full_graph.len());
+        let mut in_degrees = HashMap::with_capacity(full_graph.len());
+        let mut total_degrees = 0usize;
+        for (&key, node) in full_graph.iter() {
+            if node.dependencies.len() == 0 {
+                queue.push_back(key);
+            } else {
+                let node_dep_count = node.dependencies.len();
+                total_degrees += node_dep_count;
+                in_degrees.insert(key, node_dep_count);
+            }
+        }
+        
+        while let Some(next_key) = queue.pop_front() {
+            order.push(next_key);
+            for dep_node in full_graph[next_key].dependents.iter().copied() {
+                let Some(degrees) = in_degrees.get_mut(dep_node) else {
+                    unreachable!("in_degrees should always be valid.");
+                };
+                *degrees -= 1;
+                total_degrees -= 1;
+                if *degrees == 0 {
+                    queue.push_back(dep_node);
+                }
+            }
+        }
+        if total_degrees > 0 {
+            return Err(());
+        }
+        Ok(order)
+    }
+}
+
+#[test]
+fn dep_graph_test() {
+    let i: [Ident; 8] = [
+        syn::parse_quote!(i0),
+        syn::parse_quote!(i1),
+        syn::parse_quote!(i2),
+        syn::parse_quote!(i3),
+        syn::parse_quote!(i4),
+        syn::parse_quote!(i5),
+        syn::parse_quote!(i6),
+        syn::parse_quote!(i7),
+    ];
+    let mut graph = DepGraph::new();
+    graph.insert(&i[0], [&i[7], &i[2], &i[5]]);
+    graph.insert(&i[1], [&i[3], &i[4]]);
+    graph.insert(&i[2], [&i[6]]);
+    graph.insert(&i[3], []);
+    graph.insert(&i[4], [&i[5]]);
+    graph.insert(&i[5], []);
+    graph.insert(&i[6], [&i[5]]);
+    graph.insert(&i[7], [&i[6]]);
+    if let Ok(sort) = graph.topological_sort() {
+        assert_eq!(sort.len(), 8);
+        for node in sort {
+            println!("{node}");
+        }
+    } else {
+        panic!("Cycle detected.");
+    }
 }
 
 /*
